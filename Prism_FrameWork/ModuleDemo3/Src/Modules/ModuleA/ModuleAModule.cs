8ce67b26ac28493ad35bee9db08c035418a0938d
Arun Kumar <arun94255@gmail.com>
2019-04-04 17:10:41 +0530

    Add files via upload

---
+++ Prism_FrameWork/ModuleDemo3/Src/Modules/ModuleA/ModuleAModule.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Practices.Prism.Modularity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ModuleA
+{
+    class ModuleAModule:IModule
+    {
+        public void Initialize()
+        {
+            MessageBox.Show("Module Loaded");
+        }
+    }
+}
