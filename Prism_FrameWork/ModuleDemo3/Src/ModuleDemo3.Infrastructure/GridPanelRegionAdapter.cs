4f210a85b7d8a3051fa4d4e85fd5ae47b9de7794
Arun Kumar <arun94255@gmail.com>
2019-04-04 17:07:09 +0530

    Add files via upload

---
+++ Prism_FrameWork/ModuleDemo3/Src/ModuleDemo3.Infrastructure/GridPanelRegionAdapter.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Practices.Prism.Regions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace ModuleDemo3.Infrastructure
+{
+    public class GridPanelRegionAdapter : RegionAdapterBase<Grid>
+    {
+        public GridPanelRegionAdapter(IRegionBehaviorFactory regionBehaviorFactory)
+            : base(regionBehaviorFactory)
+        {
+
+        }
+
+
+        protected override void Adapt(IRegion region, Grid regionTarget)
+        {
+            region.Views.CollectionChanged += (s, e) =>
+            {
+                //Add
+                if (e.Action == System.Collections.Specialized.NotifyCollectionChangedAction.Add)
+                    foreach (FrameworkElement element in e.NewItems)
+                        regionTarget.Children.Add(element);
+
+                //Removal
+                if (e.Action == System.Collections.Specialized.NotifyCollectionChangedAction.Remove)
+                    foreach (FrameworkElement element in e.OldItems)
+                        if (regionTarget.Children.Contains(element))
+                            regionTarget.Children.Remove(element);
+            };
+        }
+
+        protected override IRegion CreateRegion()
+        {
+            return new AllActiveRegion();
+        }
+    }
+}
---
+++ Prism_FrameWork/ModuleDemo3/Src/ModuleDemo3.Infrastructure/RegionNames.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModuleDemo3.Infrastructure
+{
+    public class RegionNames
+    {
+        public static string TitleRegion = "TitleRegion";
+        public static string ImageRegion = "ImageRegion";
+    }
+}
